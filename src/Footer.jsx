cfb073f4c110e9f837d52242d0295fb608c87082
Raj <tanjiraj71@gmail.com>
2022-11-06 16:00:53 +0600

    my first react website version 1

---
+++ src/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="w-100 bg-light text-center">
+        <p>Mail Me At - <a href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=GTvVlcSKkkCkNGxBBlLKDlHxpbPVBpsGvSJvpGBgzPRJljNlcgVknldLprtQXvWGdXWjwXDtBXHhT" target="_blank">tanjiraj71@gmail.com</a></p>
+        <p>Phone Number - 01521329866</p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
